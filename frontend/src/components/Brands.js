7db28855fdda25f434e131184785f714dd775f90
Anukul Sharma <contactsharmaanukul@gmail.com>
2021-07-16 18:15:09 +0545

    adding changes

---
+++ frontend/src/components/Brands.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const Brands = () => {
+    return (
+        <div className='brands'>
+            <div className='small-container'>
+                <div className='row'>
+                    <div className="col-5-m">
+                        <img src="c1.jpg" alt='Kirloskar Brand' />
+                    </div>
+                    <div className="col-5-m">
+                        <img src="c2.jpg" alt='Duke Brand'/>
+                    </div>
+                    <div className="col-5-m">
+                        <img src="c3.jpg" alt='Shimge Brand'/>
+                    </div>
+                    <div className="col-5-m">
+                        <img src="c4.jpg" alt='Sharp Brand'/>
+                    </div>
+                    <div className="col-5-m">
+                        <img src="c5.jpg" alt='Varuna Brand'/>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Brands
---
+++ frontend/src/screens/AboutScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function AboutScreen() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="home.png"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>Smart Suppliers has been in business with house hold water pump since 2005. Smart Suppliers is marketing various reputed brands, makes of industrial pumps and spares since last more than 14 years. These pumps & spares are required by every industry in almost every sector.</p>
+            <p> Along with this, the company also deals in international brands of water pumps, Gas Gyser Welding machines, Vibrator Motor , Vibrators Niddle , Combine Mills & Rice mills, Bench Grinder, Cutoff Machines, Industrial Motors and other Machineries. 
+                    We have been providing technical knowhow and consulting to more than 100 industries all over Nepal this along with our supply of excellent branded products and services gives us the added advantage of ensuring high end quality and performance. We provide complete solutions for all industrial & domestic water needs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutScreen
